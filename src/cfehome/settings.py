22a28fe3d99019ac5666ad0f99a44bed7671d081
wondwessen <wessen333@gmail.com>
2024-06-26 16:47:40 +0300

    update requrement

--- src/cfehome/settings.py
+++ src/cfehome/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+from decouple import config
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,11 +20,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-x@9p^6&d%eng%d4r19n9lf2qv*(@(c=h=1@^)hj%d&#rdik+_c'
+#SECRET_KEY = 'django-insecure-x@9p^6&d%eng%d4r19n9lf2qv*(@(c=h=1@^)hj%d&#rdik+_c'
+SECRET_KEY = config("DJANGO_SECRET_KEY") 
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
+# SECURITY WARNING: don't run with debug turned on in production!
+# DEBUG = str(os.environ.get("DJANGO_DEBUG")).lower() == "true"
+DEBUG = config("DJANGO_DEBUG", cast=bool)
+print('DEBUG',DEBUG,type(DEBUG))
 ALLOWED_HOSTS = [
     ".railway.app" # https://saas.prod.railway.app
 ]
