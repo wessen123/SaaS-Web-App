7315a3aefb8b27fec901a7ad5e185cd74bf86fae
wondwessen <wessen333@gmail.com>
2024-06-26 15:02:04 +0300

    adding snippets

--- src/cfehome/settings.py
+++ src/cfehome/settings.py
@@ -25,7 +25,15 @@ SECRET_KEY = 'django-insecure-x@9p^6&d%eng%d4r19n9lf2qv*(@(c=h=1@^)hj%d&#rdik+_c
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    ".railway.app" # https://saas.prod.railway.app
+]
+if DEBUG:
+    ALLOWED_HOSTS += [
+        "127.0.0.1",
+        "localhost"
+    ]
+
 
 
 # Application definition
--- src/cfehome/views.py
+++ src/cfehome/views.py
@@ -1,10 +1,16 @@
 import pathlib
-from django.http import HttpResponse
 from django.shortcuts import render
+from django.http import HttpResponse
 
 from visits.models import PageVisit
+
 this_dir = pathlib.Path(__file__).resolve().parent
-def home_page_view(request, *args, **kwargs):
+
+def home_view(request, *args, **kwargs):
+    return about_view(request, *args, **kwargs)
+
+
+def about_view(request, *args, **kwargs):
     qs = PageVisit.objects.all()
     page_qs = PageVisit.objects.filter(path=request.path)
     try:
@@ -12,17 +18,17 @@ def home_page_view(request, *args, **kwargs):
     except:
         percent = 0
     my_title = "My Page"
+    html_template = "home.html"
     my_context = {
         "page_title": my_title,
         "page_visit_count": page_qs.count(),
         "percent": percent,
         "total_visit_count": qs.count(),
     }
-    html_template = "home.html"
     PageVisit.objects.create(path=request.path)
-    return render(request, "home.html",my_context)
+    return render(request, html_template, my_context)
 
-VALID_CODE = "abc123"
+
 def my_old_home_page_view(request, *args, **kwargs):
     my_title = "My Page"
     my_context = {
@@ -40,6 +46,3 @@ def my_old_home_page_view(request, *args, **kwargs):
     # html_file_path = this_dir / "home.html"
     # html_ = html_file_path.read_text()
     return HttpResponse(html_)
-
-VALID_CODE = "abc123"
-
